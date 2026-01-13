e92c5c01bc2f12bb7c5b4198f052294a0eb8b141
Subodh Galande <subodh.dsgn@gmail.com>
2026-01-13 19:25:01 +0530

    Refactor: Move form components to subdirectories

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,5 @@
-import { LoginForm } from "@/components/login-form";
-export default function SignupPage() {
+import { LoginForm } from "@/components/login-form/login-form";
+export default function LoginPage() {
   return (
     <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="w-full max-w-sm">
